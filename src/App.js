a27d7460ff5d6cccb43e36e145cfc78f3afe24f3
Zachary Levy <zacharyalevy@gmail.com>
2017-11-16 12:38:38 -0500

    bootstrap basic template

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ import { Switch, Route, Link } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <Link to="/odds/odds-of-getting-cancer">Cancer</Link>
-          <Link to="/odds/odds-of-dying">Dying</Link>
-          <Link to="/submit">Submit</Link>
-        </header>
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+          <a className="navbar-brand" href="/">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Link to="/odds/odds-of-getting-cancer">Cancer</Link>
+        <Link to="/odds/odds-of-dying">Dying</Link>
+        <Link to="/submit">Submit</Link>
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route path='/odds/:slug' component={Odds}/>
--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Home</h1>
       </div>
     );
   }
