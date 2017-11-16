aa245e2b9875705e3b04e4eecb5fab6750adab18
Zachary Levy <zacharyalevy@gmail.com>
2017-11-16 11:54:01 -0500

    refactor to use json data

--- src/Odds.js
+++ src/Odds.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react'
 import { PieChart, Pie, Legend, Tooltip } from 'recharts'
+import odds from './odds.json'
 
 class Odds extends Component {
-  constructor() {
-    super()
-    this.state = {
-      data02: [
-        {name: 'Odds of getting cancer', value: 50},
-        {name: 'No cancer', value: 50}
-      ]
-    }
-  }
   render() {
+    const cancerOdds = odds.find((odd) => odd.slug === "odds-of-getting-cancer")
+    console.log(cancerOdds)
     return (
       <div>
+        <h3>{cancerOdds.title}</h3>
         <PieChart width={400} height={400}>
-          <Pie data={this.state.data02} cx={200} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+          <Pie data={cancerOdds.data} cx={200} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
           <Tooltip/>
         </PieChart>
       </div>
