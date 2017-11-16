5fbecfd7499e85ec0c367ad870ff3d3a996bb974
Zachary Levy <zacharyalevy@gmail.com>
2017-11-16 12:00:48 -0500

    react router

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
